5f0ce40ea0df22845bbb6bdd440cf51738baba0c
ISAAC KOW MENSAH <ikmensah025@gmail.com>
2026-06-24 08:30:53 -0700

    Remove environment variables

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -56,10 +56,6 @@ function Contact() {
             return;
         }
 
-        console.log(process.env.REACT_APP_EMAILJS_SERVICE_ID);
-        console.log(process.env.REACT_APP_EMAILJS_TEMPLATE_ID);
-        console.log(process.env.REACT_APP_EMAILJS_PUBLIC_KEY);
-
         const templateParams = {
             name,
             email,
